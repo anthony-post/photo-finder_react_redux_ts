fd4935da82da1b9d5db2fc4d178bf8208523b8ad
anthony-post <anthony-post@HOTMAIL.COM>
2024-03-13 11:41:41 +0300

    add react router

---
+++ src/pages/home-page/home-page.tsx
@@ -0,0 +1,12 @@
+import logo from "../../logo.svg";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+};
